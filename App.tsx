562c6cf1c5f36626a71a8d5564897409ac2e2f9f
NinjaNuur <71561866+nuurcodes@users.noreply.github.com>
2022-01-24 11:09:44 +0000

    chore: add font loader

--- App.tsx
+++ App.tsx
@@ -1,26 +1,41 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthNavigator, RootNavigator } from '@screens/index';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { IconComponent } from '@components/icons';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 
 export default function App() {
-  const [authenticated, setAuthenticated] = useState(false);
+  const [ready, setReady] = useState(false);
+  const [authenticated, setAuthenticated] = useState(true);
+
+  const startApp = useCallback(async () => {
+    await SplashScreen.preventAutoHideAsync();
+
+    const fonts = [IconComponent.font];
+    const fontAssets = fonts.map((font) => Font.loadAsync(font));
+    await Promise.all([...fontAssets]);
+
+    setReady(true);
+    await SplashScreen.hideAsync();
+  }, []);
 
   useEffect(() => {
-    setTimeout(() => {
-      setAuthenticated(!authenticated);
-    }, 5000);
-  }, [authenticated]);
+    startApp();
+  }, [startApp]);
 
   return (
     <SafeAreaProvider>
       <StatusBar style='auto' />
       <GestureHandlerRootView style={{ flex: 1 }}>
-        <NavigationContainer>
-          {authenticated ? <RootNavigator /> : <AuthNavigator />}
-        </NavigationContainer>
+        {ready ? (
+          <NavigationContainer>
+            {authenticated ? <RootNavigator /> : <AuthNavigator />}
+          </NavigationContainer>
+        ) : null}
       </GestureHandlerRootView>
     </SafeAreaProvider>
   );
